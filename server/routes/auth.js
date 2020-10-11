ea92578dbf89d59a7cdff7b72e268b84beadb706
kbpsystem777 <koleen.bp@outlook.com>
2020-10-11 18:52:41 +0800

    updating routiing codes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -10,40 +10,44 @@ const authMiddleware = require("../middleware/auth")
 
 const Router = express.Router()
 
-Router.post("/signqup", async (req, res) => {
+Router.post("/signup", async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body
     const validFieldsToUpdate = ["first_name", "last_name", "email", "password"]
     const receivedFields = Object.keys(req.body)
+
     const isInvalidFieldProvided = isInvalidField(
       receivedFields,
       validFieldsToUpdate
     )
+
     if (isInvalidFieldProvided) {
       return res.status(400).send({
-        signup_error: "Invalid Field",
+        signup_error: "Invalid field.",
       })
     }
+
     const result = await pool.query(
-      `select count (*) as count from bank_user where email=$1`,
+      "select count(*) as count from bank_user where email=$1",
       [email]
     )
     const count = result.rows[0].count
     if (count > 0) {
       return res.status(400).send({
-        signup_error: "User with this email address already exists",
+        signup_error: "User with this email address already exists.",
       })
     }
+
     const hashedPassword = await bcrypt.hash(password, 8)
     await pool.query(
-      `insert into bank_user(first_name, last_name, email, password
-   ) values($1, $2, $3, $4)`,
+      "insert into bank_user(first_name, last_name, email, password) values($1,$2,$3,$4)",
       [first_name, last_name, email, hashedPassword]
     )
     res.status(201).send()
   } catch (error) {
+    console.log("err", error)
     res.status(400).send({
-      signup_error: "Error while signing up. Please try again",
+      signup_error: "Error while signing up..Try again later.",
     })
   }
 })
@@ -54,24 +58,24 @@ Router.post("/signin", async (req, res) => {
     const user = await validateUser(email, password)
     if (!user) {
       res.status(400).send({
-        signin_error: "Email and password doesnt matches",
+        sigin_error: "Email/password does not match.",
       })
     }
     const token = await generateAuthToken(user)
     const result = await pool.query(
-      "insert into token(access_token, userid) values($1, $2) returning *",
+      "insert into tokens(access_token, userid) values($1,$2) returning *",
       [token, user.userid]
     )
     if (!result.rows[0]) {
       return res.status(400).send({
-        signin_error: "Error while signing in, try again",
+        signin_error: "Error while signing in..Try again later.",
       })
     }
     user.token = result.rows[0].access_token
     res.send(user)
   } catch (error) {
     res.status(400).send({
-      signin_error: `Email/password doens't matches`,
+      signin_error: "Email/password does not match.",
     })
   }
 })
@@ -84,9 +88,11 @@ Router.post("/logout", authMiddleware, async (req, res) => {
       access_token,
     ])
     res.send()
-  } catch (err) {
+  } catch (error) {
     res.status(400).send({
-      logout_error: "Error while logging out. Please try again",
+      logout_error: "Error while logging out..Try again later.",
     })
   }
 })
+
+module.exports = Router
