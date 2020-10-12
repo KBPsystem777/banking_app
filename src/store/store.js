9b235fbcd9cb5a7326a85255aa50648312bb3caf
kbpsystem777 <koleen.bp@outlook.com>
2020-10-12 23:43:23 +0800

    building error mgmt handlers

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,15 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
-import authReducer from "../reducers/auth";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+import authReducer from "../reducers/auth"
+import errorsReducer from "../reducers/errors"
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   combineReducers({
     auth: authReducer,
+    errors: errorsReducer,
   }),
   composeEnhancers(applyMiddleware(thunk))
-);
+)
 
-export default store;
+export default store
