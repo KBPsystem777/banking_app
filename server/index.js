1a91f6e7b0923e212d9b81baab4cf72ab38b606e
kbpsystem777 <koleen.bp@outlook.com>
2020-10-13 23:28:55 +0800

    building server side support for Profile service

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const cors = require("cors")
 const authRoute = require("./routes/auth")
+const profileRoute = require("./routes/profile")
 require("dotenv").config()
-require("events").EventEmitter.defaultMaxListeners = 15
+// require("events").EventEmitter.defaultMaxListeners = 15
 
 const app = express()
 const PORT = process.env.PORT || 1993
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 1993
 app.use(express.json())
 app.use(cors())
 app.use(authRoute)
+app.use(profileRoute)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
