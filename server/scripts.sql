1bbeba3d79d7e843f80a56c5e15cad53ce0b86ca
kbpsystem777 <koleen.bp@outlook.com>
2020-10-15 23:30:28 +0800

    added DB script for adding tables

--- server/scripts.sql
+++ server/scripts.sql
@@ -2,7 +2,8 @@ CREATE DATABASE bank_account
 
 \c bank_account
 
-CREATE TABLE bank_user(
+CREATE TABLE bank_user
+(
     userid BIGSERIAL PRIMARY KEY NOT NULL,
     first_name VARCHAR(32) NOT NULL,
     last_name VARCHAR(32) NOT NULL,
@@ -11,9 +12,32 @@ CREATE TABLE bank_user(
     unique(email)
 )
 
-CREATE TABLE TOKENS(
+CREATE TABLE TOKENS
+(
     id BIGSERIAL PRIMARY KEY NOT NULL,
     access_token VARCHAR(500) NOT NULL,
     userid BIGSERIAL NOT NULL,
     FOREIGN KEY(userid) REFERENCES bank_user(userid)
 )
+
+CREATE TABLE account
+(
+    account_id BIGSERIAL PRIMARY KEY NOT NULL,
+    account_no BIGINT NOT NULL,
+    bank_name VARCHAR(50) NOT NULL,
+    ifsc VARCHAR(32) NOT NULL,
+    userid INTEGER NOT NULL,
+    total_balance BIGINT NOT NULL DEFAULT 0,
+    FOREIGN KEY(userid) REFERENCES bank_user(userid)
+);
+
+CREATE TABLE transactions
+(
+    tr_id BIGSERIAL PRIMARY KEY NOT NULL,
+    transaction_date TIMESTAMP NOT NULL,
+    withdraw_amount DECIMAL NULL,
+    deposit_amount DECIMAL NULL,
+    balance DECIMAL NOT NULL DEFAULT 0,
+    account_id BIGINT NOT NULL,
+    FOREIGN KEY(account_id) REFERENCES account(account_id)
+);
